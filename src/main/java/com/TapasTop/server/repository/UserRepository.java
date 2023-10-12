dffedc721c7d1b46ef770319fe5d975974decc72
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2023-10-12 22:36:22 +0200

    Adding findByUsername prefix

--- src/main/java/com/TapasTop/server/repository/UserRepository.java
+++ src/main/java/com/TapasTop/server/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.TapasTop.server.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import com.TapasTop.server.model.User;
@@ -9,5 +12,6 @@ import com.TapasTop.server.model.UserView;
 @RepositoryRestResource(excerptProjection = UserView.class)
 @CrossOrigin(origins = "http://localhost:8081", maxAge = 3600, allowCredentials = "true")
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  
+  Page<User> findByUsernameStartingWith(@Param("prefix") String prefix, Pageable pageable);
 }
