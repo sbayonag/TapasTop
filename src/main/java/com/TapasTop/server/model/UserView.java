5a5b207231929d037499f5f62ecb24b0ba2b2d10
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2023-10-12 22:34:20 +0200

    Custom "view" or projection for user

---
+++ src/main/java/com/TapasTop/server/model/UserView.java
@@ -0,0 +1,26 @@
+package com.TapasTop.server.model;
+
+import java.util.Date;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = {User.class}, name = "UserView")
+public interface UserView {
+
+  String getUsername();
+
+  String getEmail();
+
+  Date getCreatedAt();
+
+  String getName();
+
+  String getLastname();
+
+  String getBio();
+
+  Date birthday();
+
+  String getPicturePath();
+  
+  UserActivity getUserActivity();
+}
