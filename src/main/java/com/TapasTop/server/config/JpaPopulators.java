8537197e41d71223a43b48bc6b81c0796e7e5a32
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2023-10-12 22:32:52 +0200

    Adding testing infrastructure

---
+++ src/main/java/com/TapasTop/server/config/JpaPopulators.java
@@ -0,0 +1,20 @@
+package com.TapasTop.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+
+@Configuration
+public class JpaPopulators {
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() throws Exception {
+        Jackson2RepositoryPopulatorFactoryBean factory =
+                new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[] {new ClassPathResource("user-data.json"),
+                new ClassPathResource("dish-data.json")});
+        return factory;
+    }
+}
