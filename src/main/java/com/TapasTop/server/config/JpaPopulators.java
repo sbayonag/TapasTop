b284010c2b191d1bd73c85aa854f1c74dffd2c54
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2023-10-17 22:25:29 +0200

    Fixing "LocalDate" De/Serialization in jpapopulator

--- src/main/java/com/TapasTop/server/config/JpaPopulators.java
+++ src/main/java/com/TapasTop/server/config/JpaPopulators.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class JpaPopulators {
 
     @Bean
-    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() throws Exception {
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator(ObjectMapper objectMapper)
+            throws Exception {
         Jackson2RepositoryPopulatorFactoryBean factory =
                 new Jackson2RepositoryPopulatorFactoryBean();
         factory.setResources(new Resource[] {new ClassPathResource("01-user-data.json"),
                 new ClassPathResource("02-dish-data.json")});
+        factory.setMapper(objectMapper);
         return factory;
     }
 }
