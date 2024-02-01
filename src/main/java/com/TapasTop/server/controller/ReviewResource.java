9552e4497ed5a2f3a48b172abd9a0cea8be164b0
BAYONA GARAVITO, SEBASTIAN <sebastian.bayonag@alumnos.upm.es>
2024-01-31 21:43:49 -0500

    Removing manual POST operation in Review controller

--- src/main/java/com/TapasTop/server/controller/ReviewResource.java
+++ src/main/java/com/TapasTop/server/controller/ReviewResource.java
@@ -1,44 +1,13 @@
 package com.TapasTop.server.controller;
 
-import com.TapasTop.server.exception.DishNotFoundException;
-import com.TapasTop.server.exception.UserNotFoundException;
-import com.TapasTop.server.model.Dish;
-import com.TapasTop.server.model.Review;
-import com.TapasTop.server.model.User;
-import com.TapasTop.server.repository.DishRepository;
-import com.TapasTop.server.repository.ReviewRepository;
-import com.TapasTop.server.repository.UserRepository;
+import com.TapasTop.server.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @BasePathAwareController("/reviews")
 public class ReviewResource {
 
   @Autowired
-  private static ReviewRepository reviewRepository;
+  ReviewService reviewService;
 
-  @Autowired
-  private static UserRepository userRepository;
-
-  @Autowired
-  private static DishRepository dishRepository;
-
-  @PostMapping
-  ResponseEntity<?> createReview(@RequestBody ReviewForm reviewForm)
-      throws UserNotFoundException, DishNotFoundException {
-    User user = userRepository.findById(reviewForm.userId())
-        .orElseThrow(() -> new UserNotFoundException(reviewForm.userId()));
-    Dish dish = dishRepository.findById(reviewForm.dishId())
-        .orElseThrow(() -> new DishNotFoundException(reviewForm.dishId()));
-    Review review = new Review(reviewForm.rate(), user, dish);
-    review = reviewRepository.save(review);
-    user.addReview(review);
-    dish.addReview(review);
-    EntityModel<Review> resource = EntityModel.of(review);
-    return ResponseEntity.ok(resource);
-  }
 }
